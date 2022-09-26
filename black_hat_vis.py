9b4d03ba7502dafb64087ba60f51b42824d12638
Joachim <Joachim@brendborg.dk>
2022-09-26 16:57:19 +0200

    Made shit work

--- black_hat_vis.py
+++ black_hat_vis.py
@@ -1,4 +1,22 @@
-import altair
+import altair as alt
+import pandas as pd
+from vega_datasets import data
+
+cars = data.cars()
+
+chart = alt.Chart(cars).mark_point().encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin',
+).interactive()
+
+chart.show()
 
 
-print('Hello, world')
+data = pd.read_csv("denver_avgtemp.csv")
+
+
+alt.Chart(data.reset_index()).mark_line().encode(
+    x='index:T',
+    y='value:Q'
+)
