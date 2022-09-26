a1afe71080c108befcb02eed13ab3c0fb1f0a79f
Asger Ullersted Rasmussen <201806934@post.au.dk>
2022-09-26 19:58:12 +0200

    flipped axis

--- black_hat_vis.py
+++ black_hat_vis.py
@@ -2,21 +2,14 @@ import altair as alt
 import pandas as pd
 from vega_datasets import data
 
-cars = data.cars()
 
-chart = alt.Chart(cars).mark_point().encode(
-    x='Horsepower',
-    y='Miles_per_Gallon',
-    color='Origin',
-).interactive()
-
-chart.show()
 
 
 data = pd.read_csv("denver_avgtemp.csv")
 
-
-alt.Chart(data.reset_index()).mark_line().encode(
-    x='index:T',
-    y='value:Q'
-)
+print(data)
+chart = alt.Chart(data).mark_line().encode(
+    x='year:O',
+    y=alt.Y('avg_temp:Q', scale=alt.Scale(reverse=True))
+).interactive()
+chart.show()
